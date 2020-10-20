9dc1d8cb0b44d4b92661b8a16661090eccddcf25
Henry Herrington <35976857+henryherrington@users.noreply.github.com>
2020-10-20 00:37:12 -0400

    Navbar and Workout Update
    Worked on Navbar, started updating workout page

--- ludis-react-app/src/components/Navigation.js
+++ ludis-react-app/src/components/Navigation.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import '../css/Navigation.css';
-
-import { NavLink } from 'react-router-dom';
-// // import Button from '@material-ui/core/Button';
-// import Drawer from '@material-ui/core/Drawer';
-
- 
 import clsx from 'clsx';
 import '../css/Navigation.css';
 import { NavLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 
 // icons
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import MailIcon from '@material-ui/icons/Mail';
+import FitnessCenterIcon from '@material-ui/icons/FitnessCenter';
+import PeopleIcon from '@material-ui/icons/People';
+import SettingsIcon from '@material-ui/icons/Settings';
 import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
 
 const useStyles = makeStyles({
   list: {
-    width: 250,
+    width: 275,
+    textDecoration: 'none',
+    color: 'black'
   },
   fullList: {
     width: 'auto',
@@ -55,39 +50,59 @@ export default function TemporaryDrawer() {
     setState({ ...state, [anchor]: open });
   };
 
-  const icons = ['HomeIcon'];
-
+  const pages = {
+    home: {
+      title: 'Home',
+      link: 'home',
+      icon: <HomeIcon/>
+    },
+    workouts: {
+      title: 'Workouts',
+      link: 'workouts',
+      icon: <FitnessCenterIcon/>
+    },
+    leaderboards: {
+      title: 'Leaderboards',
+      link: 'leaderboards',
+      icon: <PeopleIcon/>
+    },
+    settings: {
+      title: 'Settings',
+      link: 'settings',
+      icon: <SettingsIcon/>
+    }
+  };
+  
   const list = (anchor) => (
     <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
+      className={clsx(classes.list)}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
+      <div id="profile-wrapper">
+        <img id="nav-profile-avatar" src="default-avatar.png"></img>
+        <div id="user-name">
+          Gilron Tsabkevich
+        </div>
+        <div id="user-team">
+          Princeton University Track & Field
+        </div>
+      </div>
       <List>
-        {['home', 'workouts', 'leaderboards'].map((text, index) => (
-          <NavLink to={text}>
-          <ListItem button key={text}>
-            <ListItemIcon>{<HomeIcon/>}</ListItemIcon>
-            <ListItemText primary={text} />
+        {['home', 'workouts', 'leaderboards', 'settings'].map((text) => (
+          <NavLink to={pages[text].link} className="nav-link">
+            <Divider />
+          <ListItem button key={text} >
+            <ListItemIcon>{pages[text].icon}</ListItemIcon>
+            <ListItemText primary={pages[text].title} />
           </ListItem>
         </NavLink>
         ))}
+        <Divider />
       </List>
-      <Divider />
 
-      <List>
-        {['settings'].map((text, index) => (
-          <NavLink to={text}>
-          <ListItem button key={text}>
-            <ListItemIcon>{<MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        </NavLink>
-        ))}
-      </List>
+
     </div>
   );
 
@@ -106,100 +121,4 @@ export default function TemporaryDrawer() {
     </div>
   );
 
-}
-
- 
-// export default Navigation;
-
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-
-// icons
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import MailIcon from '@material-ui/icons/Mail';
-import MenuIcon from '@material-ui/icons/Menu';
-import HomeIcon from '@material-ui/icons/Home';
-
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
-
-export default function TemporaryDrawer() {
-  const classes = useStyles();
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-
-  const icons = ['HomeIcon'];
-
-  const list = (anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
-      })}
-      role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}
-    >
-      <List>
-        {['home', 'workouts', 'leaderboards'].map((text, index) => (
-          <NavLink to={text}>
-          <ListItem button key={text}>
-            <ListItemIcon>{<HomeIcon/>}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        </NavLink>
-        ))}
-      </List>
-      <Divider />
-
-      <List>
-        {['settings'].map((text, index) => (
-          <NavLink to={text}>
-          <ListItem button key={text}>
-            <ListItemIcon>{<MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        </NavLink>
-        ))}
-      </List>
-    </div>
-  );
-
-  return (
-    <div>
-      {['left'].map((anchor) => (
-        <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}><MenuIcon /></Button>
-          <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
-            {list(anchor)}
-          </Drawer>
-        </React.Fragment>
-      ))}
-    </div>
-  );
 }
--- ludis-react-app/src/components/Workouts.js
+++ ludis-react-app/src/components/Workouts.js
@@ -2,14 +2,57 @@ import React from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Chip from '@material-ui/core/Chip';
+
+// list imports
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+import '../css/Workouts.css';
  
 const Workouts = () => {
+
+   const [open, setOpen] = React.useState(true);
+   const handleClick = () => {
+      setOpen(!open);
+    };
+  
+
     return (
        <div>
-
-         <h1>Workouts</h1>
-         <p>Here are your workouts.</p>
-         <InputLabel id="label">Age</InputLabel>
+         <h1>New Workout</h1>
+         <form>
+            <div class="main-input-title">Workout Name<br></br>
+            <Input placeholder="for example: Pre Meet" fullWidth />
+            </div>
+            <div class="main-input-title">Workout Description<br></br> 
+            <Input placeholder="important things to remember" fullWidth />
+            </div>
+            <div class="main-input-title">Tags<br></br> 
+            <Input placeholder="for example: technical" inputProps={{ }} />
+            <div id="tags-container">
+               <Chip label="Technical" onDelete/>
+               <Chip label="Low Intensity" onDelete/>
+               <Chip label="Full Team" onDelete/>
+            </div>
+            </div>
+         </form>
+         
+   <List className="drill-header">
+      <ListItem button onClick={handleClick} >
+        <ListItemText primary="Inbox"/>
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button>
+            <ListItemText primary="Starred" />
+            <InputLabel id="label">Age</InputLabel>
          <Select labelId="label" id="select">
             <MenuItem value="10">Ten</MenuItem>
             <MenuItem value="20">Twenty</MenuItem>
@@ -19,6 +62,10 @@ const Workouts = () => {
             <MenuItem value="10">Ten</MenuItem>
             <MenuItem value="20">Twenty</MenuItem>
          </Select>
+          </ListItem>
+        </List>
+      </Collapse>
+    </List>
 
        </div>
     );
