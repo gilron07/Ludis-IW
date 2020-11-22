6a444d671cfd63c55cf239668e3e249475673d74
Henry Herrington <35976857+henryherrington@users.noreply.github.com>
2020-11-22 10:36:45 -0500

    calendar update

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -2,11 +2,12 @@ import { createMuiTheme }  from '@material-ui/core/styles'
 const theme = createMuiTheme({
   palette: {
     primary: {
-        main: '#f2ae1b'
+        main: '#f2ae1b',
+        light: "f4a215"
     },
     secondary: {
         main: '#7ed9b1'
-    } 
+    },
   },
 })
 export default theme
